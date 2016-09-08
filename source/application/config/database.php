7f995c33d6a654ed48d38369c5da5457d19d6a1f
Nathan Nguyen <nhan1110@gmail.com>
2016-09-08 10:43:58 +0700

    Clear data connect database

--- source/application/config/database.php
+++ source/application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'vnmarket';
-$db['default']['password'] = 'adm1n@16';
-$db['default']['database'] = 'vnmarket';
+$db['default']['username'] = 'xxxxxx';
+$db['default']['password'] = 'xxxxxx';
+$db['default']['database'] = 'xxxxxx';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
